86f0aec92eae4e8e0f0b921b3983ca6bf0966947
val-era <ignatyev_valera@mail.ru>
2022-12-20 11:43:12 +0300

    Create sql table

--- Planner/mainpage/models.py
+++ Planner/mainpage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class Tasks(models.Model):
+    user_name = models.TextField("Имя")
+    title = models.TextField("Название задачи")
+    task = models.TextField("Задача")
+    teg = models.TextField("Тэг")
+    date = models.DateField('Дата Задачи')
+    is_global = models.BooleanField('is_global_task')
+    global_task = models.TextField("Глобальная задача")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
 
