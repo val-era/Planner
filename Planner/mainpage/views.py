ee893469ed06683daa696eeb81acf84bf44ae9f4
val-era <ignatyev_valera@mail.ru>
2022-12-20 16:17:48 +0300

    Create page with tasks. Add task on main page

--- Planner/mainpage/views.py
+++ Planner/mainpage/views.py
@@ -1,8 +1,12 @@
+import datetime
+
 from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Tasks
 
 
 def main(request):
-    return render(request, 'mainpage/main.html')
+    task = Tasks.objects.order_by("date").filter(date=datetime.date.today())
+    return render(request, 'mainpage/main.html', {'task': task})
