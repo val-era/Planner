5da73a7ebc257124806e92b1f8fd11ede8df0814
val-era <ignatyev_valera@mail.ru>
2022-12-22 21:05:26 +0300

    update forms for view in html

--- Planner/mainpage/forms.py
+++ Planner/mainpage/forms.py
@@ -1,7 +1,28 @@
+import datetime
+
 from .models import Tasks
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateInput
+
+
 
 class TaskForm(ModelForm):
     class Meta:
         model = Tasks
         fields = ['title', 'task', 'teg', 'date']
+
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Заголовок задачи',
+            }),
+            "task": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Задача',
+            }),
+            "date": DateInput(attrs={
+                'type': 'date',
+                'value': datetime.date.today(),
+                'min': "2022-01-01",
+                'max': "2030-12-31",
+            })
+        }
