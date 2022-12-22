a194b695ec7b81502e076eaf2a3a5c86096dc177
val-era <ignatyev_valera@mail.ru>
2022-12-22 21:03:47 +0300

    Make migrations

--- Planner/mainpage/static/mainpage/java/plan.js
+++
@@ -1,9 +0,0 @@
-var elements = $('.modal-overlay, .modal');
-
-$('button').click(function(){
-    elements.addClass('active');
-});
-
-$('.close-modal').click(function(){
-    elements.removeClass('active');
-});
