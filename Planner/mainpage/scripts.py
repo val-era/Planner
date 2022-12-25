82b7c25786e4bdbce52125c42ca20e16ad6c8c34
val-era <ignatyev_valera@mail.ru>
2022-12-25 12:59:41 +0300

    add scripts.py where add recurring scripts and function

---
+++ Planner/mainpage/scripts.py
@@ -0,0 +1,28 @@
+import datetime
+from .models import Tasks
+from .forms import TaskForm, DateForm, GlobalForm
+
+
+def save(title, task, teg, date):
+    Tasks.objects.create(
+        title=title,
+        task=task,
+        teg=teg,
+        date=date,
+        is_global=False,
+        is_archive=False
+    )
+
+
+def update_global(select, global_task, gl_id):
+    if select != "" and global_task == "":
+        Tasks.objects.filter(id=gl_id).update(global_task=select)
+        Tasks.objects.filter(id=gl_id).update(is_global=True)
+    elif select != "" and global_task != "":
+        Tasks.objects.filter(id=gl_id).update(global_task=global_task)
+        Tasks.objects.filter(id=gl_id).update(is_global=True)
+    elif select == "" and global_task != "":
+        Tasks.objects.filter(id=gl_id).update(global_task=global_task)
+        Tasks.objects.filter(id=gl_id).update(is_global=True)
+    else:
+        pass
