671330e5359f712b09b51b5127f426996d10b12f
val-era <ignatyev_valera@mail.ru>
2022-12-25 13:00:20 +0300

    update files, add function to see weekly tasks and monthly tasks

--- Planner/mainpage/urls.py
+++ Planner/mainpage/urls.py
@@ -4,8 +4,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path(r'archive/<parameter>', views.archive, name='archive'),
-    path(r'del/<parameter>', views.delite, name='del'),
+    path(r'archive/<parameter>/<parameter2>', views.archive, name='archive'),
+    path(r'del/<parameter>/<parameter2>', views.delite, name='del'),
     path('archive_tasks', views.archive_tasks, name='archive_tasks'),
     path(r'del_archive/<parameter>', views.delite_archive, name='del_archive'),
+    path('tomorrow', views.tomorrow, name='tomorrow'),
+    path('week', views.week, name='week'),
+    path('month', views.month, name='month'),
 ]
--- Planner/mainpage/views.py
+++ Planner/mainpage/views.py
@@ -6,17 +6,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Tasks
 from .forms import TaskForm, DateForm, GlobalForm
-
-
-def save(title, task, teg, date):
-    Tasks.objects.create(
-        title=title,
-        task=task,
-        teg=teg,
-        date=date,
-        is_global=False,
-        is_archive=False
-    )
+from .scripts import save, update_global
 
 
 def main(request):
@@ -41,18 +31,7 @@ def main(request):
         gl_id = glob_form["idtask"].value()
         select = glob_form["sel"].value()
         global_task = glob_form["global_add"].value()
-        if select != "" and global_task == "":
-            Tasks.objects.filter(id=gl_id).update(global_task=select)
-            Tasks.objects.filter(id=gl_id).update(is_global=True)
-        elif select != "" and global_task != "":
-            Tasks.objects.filter(id=gl_id).update(global_task=global_task)
-            Tasks.objects.filter(id=gl_id).update(is_global=True)
-        elif select == "" and global_task != "":
-            Tasks.objects.filter(id=gl_id).update(global_task=global_task)
-            Tasks.objects.filter(id=gl_id).update(is_global=True)
-        else:
-            pass
-
+        update_global(select, global_task, gl_id)
 
     task = Tasks.objects.order_by("date").filter(date=datetime.date.today())
     global_tasks = set([i.global_task for i in Tasks.objects.order_by("date")])
@@ -60,17 +39,38 @@ def main(request):
     dateform = DateForm()
     globform = GlobalForm()
     return render(request, 'mainpage/plan.html', {'task': task, 'form': form, 'dateform': dateform,
-                                                  'globform': globform, 'global_tasks': global_tasks})
+                                                  'globform': globform, 'global_tasks': global_tasks, 'day': 'today'})
 
 
-def archive(request, parameter):
-    Tasks.objects.filter(id=parameter).update(is_archive=True)
-    return redirect('main')
+def archive(request, parameter, parameter2):
+    if parameter2 == 'today':
+        Tasks.objects.filter(id=parameter).update(is_archive=True)
+        return redirect('main')
+    elif parameter2 == 'week':
+        Tasks.objects.filter(id=parameter).update(is_archive=True)
+        return redirect('week')
+    elif parameter2 == 'month':
+        Tasks.objects.filter(id=parameter).update(is_archive=True)
+        return redirect('month')
+    else:
+        Tasks.objects.filter(id=parameter).update(is_archive=True)
+        return redirect('tomorrow')
 
 
-def delite(request, parameter):
-    Tasks.objects.filter(id=parameter).delete()
-    return redirect('main')
+def delite(request, parameter, parameter2):
+    if parameter2 == 'today':
+        Tasks.objects.filter(id=parameter).delete()
+        return redirect('main')
+    elif parameter2 == 'week':
+        Tasks.objects.filter(id=parameter).delete()
+        return redirect('week')
+    elif parameter2 == 'month':
+        Tasks.objects.filter(id=parameter).delete()
+        return redirect('month')
+    else:
+        Tasks.objects.filter(id=parameter).delete()
+        return redirect('tomorrow')
+
 
 def archive_tasks(request):
     if request.method == 'POST':
@@ -88,7 +88,121 @@ def archive_tasks(request):
     dateform = DateForm()
     return render(request, 'mainpage/archive_tasks.html', {'task': task, 'dateform': dateform})
 
+
 def delite_archive(request, parameter):
     Tasks.objects.filter(id=parameter).delete()
     return redirect('archive_tasks')
+
+
+def tomorrow(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            title = form["title"].value()
+            task = form["task"].value()
+            teg = form["teg"].value()
+            date = form["date"].value()
+            save(title, task, teg, date)
+            return redirect('tomorrow')
+        else:
+            pass
+
+        time_form = DateForm(request.POST)
+        id = time_form["id"].value()
+        date = time_form["date"].value()
+        Tasks.objects.filter(id=id).update(date=date)
+
+        glob_form = GlobalForm(request.POST)
+        gl_id = glob_form["idtask"].value()
+        select = glob_form["sel"].value()
+        global_task = glob_form["global_add"].value()
+        update_global(select, global_task, gl_id)
+
+    task = Tasks.objects.order_by("date").filter(date=(datetime.date.today() + datetime.timedelta(1)))
+    global_tasks = set([i.global_task for i in Tasks.objects.order_by("date")])
+    form = TaskForm()
+    dateform = DateForm()
+    globform = GlobalForm()
+    return render(request, 'mainpage/plan.html', {'task': task, 'form': form, 'dateform': dateform,
+                                                  'globform': globform, 'global_tasks': global_tasks, 'day': 'tomorrow'})
+
+
+def week(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            title = form["title"].value()
+            task = form["task"].value()
+            teg = form["teg"].value()
+            date = form["date"].value()
+            save(title, task, teg, date)
+            return redirect('week')
+        else:
+            pass
+
+        time_form = DateForm(request.POST)
+        id = time_form["id"].value()
+        date = time_form["date"].value()
+        Tasks.objects.filter(id=id).update(date=date)
+
+        glob_form = GlobalForm(request.POST)
+        gl_id = glob_form["idtask"].value()
+        select = glob_form["sel"].value()
+        global_task = glob_form["global_add"].value()
+        update_global(select, global_task, gl_id)
+    tasks = Tasks.objects.order_by("date").filter(date__range=[datetime.date.today(), datetime.date.today() + datetime.timedelta(7)])
+    tasks = tasks.filter(is_archive=False)
+    task = {}
+    for i in tasks:
+        if i.date not in task:
+            task[i.date] = [i]
+        else:
+            task[i.date].append(i)
+    global_tasks = set([i.global_task for i in Tasks.objects.order_by("date")])
+    form = TaskForm()
+    dateform = DateForm()
+    globform = GlobalForm()
+    return render(request, 'mainpage/days.html', {'task': task, 'form': form, 'dateform': dateform,
+                                                  'globform': globform, 'global_tasks': global_tasks,
+                                                  'day': 'week'})
+
+
+def month(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            title = form["title"].value()
+            task = form["task"].value()
+            teg = form["teg"].value()
+            date = form["date"].value()
+            save(title, task, teg, date)
+            return redirect('month')
+        else:
+            pass
+
+        time_form = DateForm(request.POST)
+        id = time_form["id"].value()
+        date = time_form["date"].value()
+        Tasks.objects.filter(id=id).update(date=date)
+
+        glob_form = GlobalForm(request.POST)
+        gl_id = glob_form["idtask"].value()
+        select = glob_form["sel"].value()
+        global_task = glob_form["global_add"].value()
+        update_global(select, global_task, gl_id)
+    tasks = Tasks.objects.order_by("date").filter(date__range=[datetime.date.today(), datetime.date.today() + datetime.timedelta(31)])
+    tasks = tasks.filter(is_archive=False)
+    task = {}
+    for i in tasks:
+        if i.date not in task:
+            task[i.date] = [i]
+        else:
+            task[i.date].append(i)
+    global_tasks = set([i.global_task for i in Tasks.objects.order_by("date")])
+    form = TaskForm()
+    dateform = DateForm()
+    globform = GlobalForm()
+    return render(request, 'mainpage/days.html', {'task': task, 'form': form, 'dateform': dateform,
+                                                  'globform': globform, 'global_tasks': global_tasks,
+                                                  'day': 'month'})
 
